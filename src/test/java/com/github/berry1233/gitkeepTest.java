88da1063a3e2ce4b414a584647928979943d9ccd
PC234 <rose01205@gmail.com>
2017-12-06 12:06:59 +0800

    i am handsome

---
+++ src/test/java/com/github/berry1233/gitkeepTest.java
@@ -0,0 +1,12 @@
+package com.github.berry1233;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class gitkeepTest {
+    @Test
+    public void add() throws Exception {
+    }
+
+}
